f600e751f4588adf09600bce4a0ebd6fe6ced474
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-18 00:34:46 -0700

    Major UI overhaul

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function teamClick() {
+  window.location.href = "team.html";
+}
+
+function homeClick() {
+  window.location.href = "index.html";
+}
+
+function whatWeDoClick() {
+  window.location.href = "what-we-do.html";
+}
+
+function contactClick() {
+  window.location.href = "contact.html";
+}
+
+function historyClick() {
+  window.location.href = "history.html";
+}
+
+$(function(){
+  $('.profilepic').on('click', function(e){
+    var $biginfo = $('#teamcontent');
+    var $bigname = $('#bigname');
+    var $bigjob  = $('#bigjob');
+    var $bigdesc = $('#bigdesc');
+
+    var newname = $(this).attr('alt');
+    var newrole = $(this).siblings('.job').eq(0).html();
+    var newdesc = $(this).siblings('.desc').eq(0).html();
+
+    $bigname.html(newname);
+    $bigjob.html(newrole);
+    $bigdesc.html(newdesc);
+
+    if($biginfo.css('display') == 'none') {
+      $biginfo.slideDown(350);
+    }
+  });
+});
